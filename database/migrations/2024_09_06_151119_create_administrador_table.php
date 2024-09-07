177043d82927ba9c4823377f3b1048542cfc781d
JosCraft <vjsuxo@umsa.bo>
2024-09-07 08:32:09 -0400

    cambio en error de migracions

--- database/migrations/2024_09_06_151119_create_administrador_table.php
+++ database/migrations/2024_09_06_151119_create_administrador_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('compradores', function (Blueprint $table) {
+        Schema::create('administradores', function (Blueprint $table) {
             $table->id(); // PK separado
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); // Relación uno a uno con 'users'
             $table->timestamps();
